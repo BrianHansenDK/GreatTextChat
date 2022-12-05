5979cab56742a27b4ad7d360c05d691757cf3f60
GreyhatDemonlord <brianhansen.work@gmail.com>
2022-12-05 06:42:31 +0100

    Created reusable editable input

---
+++ src/components/EditableInput.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useCallback } from 'react'
+import { Alert, Icon, Input, InputGroup } from 'rsuite'
+
+const EditableInput = ({ initialValue, onSave, label = null, placeholder = 'Right your value', emptymsg = 'Input is empty', ...inputProps }) => {
+
+    const [input, setInput] = useState(initialValue)
+    const [isEditable, setIsEditable] = useState(false)
+
+    const onInputChange = useCallback(
+        (value) => {
+            setInput(value)
+        },
+        [],
+    )
+
+    const onEditClick = useCallback(
+        () => {
+            setIsEditable(p => !p)
+            setInput(initialValue)
+        },
+        [initialValue],
+    )
+
+    const onSaveClick = async () => {
+        const trimmedValue = input.trim()
+
+        if (trimmedValue === '') {
+            Alert.info(emptymsg, 4000)
+        }
+
+        if (trimmedValue !== initialValue) {
+            await onSave(trimmedValue)
+        }
+
+        setIsEditable(false)
+    }
+
+
+    return (
+        <div>
+            {label}
+            <InputGroup>
+                <Input
+                    {...inputProps}
+                    disabled={!isEditable}
+                    placeholder={placeholder}
+                    value={input}
+                    onChange={onInputChange}
+                />
+                <InputGroup.Button onClick={onEditClick}>
+                    <Icon
+                        icon={isEditable ? 'close' : 'edit2'}
+                    />
+                </InputGroup.Button>
+                {isEditable ? (
+                    <InputGroup.Button onClick={onSaveClick}>
+                        <Icon
+                            icon='check'
+                        />
+                    </InputGroup.Button>
+                ) : null}
+            </InputGroup>
+
+        </div>
+    )
+}
+
+export default EditableInput
--- src/components/dashboard/Index.jsx
+++ src/components/dashboard/Index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Button, Drawer } from 'rsuite'
+import { Button, Divider, Drawer } from 'rsuite'
 import { useProfile } from '../../context/profile.context'
+import EditableInput from '../EditableInput'
 
 const Dashboard = ({ onSignOut }) => {
 
     const { profile } = useProfile()
+    const onSave = async newData => {
+        console.log(newData)
+    }
 
     return (
         <>
@@ -18,8 +22,14 @@ const Dashboard = ({ onSignOut }) => {
                 <h3>
                     Hey {profile.name} 🚀
                 </h3>
+                <Divider />
+                <EditableInput
+                    name='Nickname'
+                    initialValue={profile.name}
+                    onSave={onSave}
+                    label={<h6 className='mb-2'>Nickname</h6>}
+                />
             </Drawer.Body>
-
             <Drawer.Footer>
                 <Button block color='red' onClick={onSignOut}>
                     Sign Out

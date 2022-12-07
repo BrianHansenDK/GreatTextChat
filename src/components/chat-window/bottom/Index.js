2026be8d65693fa9e11d68145f4793a0aada828d
GreyhatDemonlord <brianhansen.work@gmail.com>
2022-12-07 12:40:01 +0100

    Created chat bottom, which is an input and a button you can use to send messages to the chat room

--- src/components/chat-window/bottom/Index.js
+++ src/components/chat-window/bottom/Index.js
@@ -1,8 +1,92 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import { Alert, Icon, Input, InputGroup } from 'rsuite'
+import firebase from 'firebase/app'
+import { useParams } from 'react-router'
+import { useProfile } from '../../../context/profile.context'
+import { database } from '../../../misc/firebase'
+
+function assembleMessage(profile, chatId) {
+    return {
+        roomId: chatId,
+        author: {
+            name: profile.name,
+            uid: profile.uid,
+            createdAt: profile.createdAt,
+            ...(profile.avatar ? { avatar: profile.avatar } : {})
+        },
+        createdAt: firebase.database.ServerValue.TIMESTAMP
+    }
+}
 
 const Bottom = () => {
+
+    const [input, setInput] = useState()
+    const [loading, setLoading] = useState(false)
+
+    const { profile } = useProfile()
+    const { chatId } = useParams()
+
+    const onInputChange = useCallback(
+        (value) => {
+            setInput(value)
+        },
+        [],
+    )
+
+    const sendMessage = async () => {
+        if (input.trim() === '') {
+            return
+        }
+
+        const msgData = assembleMessage(profile, chatId)
+        msgData.text = input
+
+        const updates = {}
+
+        const messageId = database.ref('messages').push().key
+        updates[`/messages/${messageId}`] = msgData
+        updates[`/rooms/${chatId}/lastMessage`] = {
+            ...msgData,
+            msgId: messageId,
+        }
+        setLoading(true)
+        try {
+            await database.ref().update(updates)
+            setInput('')
+            setLoading(false)
+        } catch (err) {
+            setLoading(false)
+            Alert.error(err.message, 4000)
+        }
+    }
+
+    const onKeyDown = (ev) => {
+        if (ev.keyCode === 13) {
+            ev.preventDefault()
+            sendMessage()
+        }
+    }
+
+
     return (
-        <div>Bottom</div>
+        <div>
+            <InputGroup>
+                <Input
+                    value={input}
+                    onChange={onInputChange}
+                    placeholder='Write a new message...'
+                    onKeyDown={onKeyDown}
+                />
+                <InputGroup.Button
+                    color='blue'
+                    appearance='primary'
+                    onClick={sendMessage}
+                    disabled={loading}
+                >
+                    <Icon icon='send' />
+                </InputGroup.Button>
+            </InputGroup>
+        </div>
     )
 }
 

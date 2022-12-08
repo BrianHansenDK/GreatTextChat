10ef1e592c122f8863e024042319e93b28edb537
GreyhatDemonlord <brianhansen.work@gmail.com>
2022-12-08 07:30:38 +0100

    Added programmatical hover

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import { useHover } from '../../../misc/CustomHooks';
 import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
@@ -10,6 +11,7 @@ import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 const MessageItem = ({ message, handleAdmin }) => {
 
     const { author, createdAt, text } = message
+    const [hoverRef, isHovered] = useHover()
 
     const isAdmin = useCurrentRoom(v => v.isAdmin)
     const admins = useCurrentRoom(v => v.admins)
@@ -19,7 +21,7 @@ const MessageItem = ({ message, handleAdmin }) => {
     const canGrantAdmin = isAdmin && !isAuthor
 
     return (
-        <li className='padded mb-1'>
+        <li className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02' : ''} `} ref={hoverRef}>
             <div className='d-flex align-items-center font-bolder mb-1'>
                 <PresenceDot uid={author.uid} />
                 <ProfileAvatar src={author.avatar} name={author.name} className='ml-1' size='xs' />

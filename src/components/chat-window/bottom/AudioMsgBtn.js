ea383ab47e8705d822cf1807a798c8b1f9877bab
GreyhatDemonlord <brianhansen.work@gmail.com>
2022-12-09 13:50:46 +0100

    Added option to record and upload audio files

---
+++ src/components/chat-window/bottom/AudioMsgBtn.js
@@ -0,0 +1,54 @@
+import React, { useCallback, useState } from 'react'
+import { useParams } from 'react-router'
+import { Alert, Icon, InputGroup } from 'rsuite'
+import { ReactMic } from 'react-mic'
+import { storage } from '../../../misc/firebase'
+
+const AudioMsgBtn = ({ afterUpload }) => {
+
+    const { chatId } = useParams()
+    const [recording, setRecording] = useState(false)
+    const [uploading, setUploading] = useState(false)
+
+    const micHandler = useCallback(() => {
+        setRecording(p => !p)
+    }, [])
+
+    const onUpload = useCallback(async (data) => {
+        setUploading(true)
+        try {
+            const snap = await storage
+                .ref(`/chat/${chatId}`)
+                .child(`audio_${Date.now()}.mp3`)
+                .put(data.blobFile, { cacheControl: `public, max-age=${3600 * 24 * 3}` })
+
+            const file = {
+                contentType: snap.metadata.contentType,
+                name: snap.metadata.name,
+                url: await snap.ref.getDownloadURL()
+            }
+
+            setUploading(false)
+            afterUpload([file])
+        } catch (err) {
+            setUploading(false)
+            Alert.error(err.message, 4000)
+        }
+    }, [afterUpload, chatId])
+
+
+
+    return (
+        <InputGroup.Button onClick={micHandler} disabled={uploading} className={recording ? 'animate-blink' : ''}>
+            <Icon icon='microphone' />
+            <ReactMic
+                record={recording}
+                className="d-none"
+                onStop={onUpload}
+                mimeType='audio/mp3'
+            />
+        </InputGroup.Button>
+    )
+}
+
+export default AudioMsgBtn
